d8b0352fa390bddf95f17d977ab3848cd63c27bf
Owen Huggins <owenscotthuggins@gmail.com>
2023-08-28 21:43:39 -0400

    asdf

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import Testing # Replace with the correct name and location of your script file
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,4 +13,6 @@ def run_script():
     return "Script has been run!"
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run()
